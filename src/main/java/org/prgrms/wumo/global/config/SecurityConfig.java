4353d60214cb638ebac7047f015b88da1a778253
ttaehee <ttaehee1105@gmail.com>
2023-02-22 20:39:27 +0900

    Chore : Security에서 Swagger 경로 제외

---
+++ src/main/java/org/prgrms/wumo/global/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package org.prgrms.wumo.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.httpBasic().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authorizeHttpRequests()
+			.antMatchers("/swagger-ui.html").permitAll()
+			.and();
+
+		return http.build();
+	}
+}
