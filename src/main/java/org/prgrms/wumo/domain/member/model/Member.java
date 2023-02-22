b2104803d1fb2e4b51a6151763af26c28b166dfd
Taehee Kim <ttaehee1105@gmail.com>
2023-02-22 22:01:43 +0900

    [WUMO-7] Member 엔티티 구현 (#1)
    * Feat : [WUMO-98] Email embedded type 구현
    
    * Feat : [WUMO-99] Password embedded type 구현
    
    * Feat : [WUMO-89] Member entity

---
+++ src/main/java/org/prgrms/wumo/domain/member/model/Member.java
@@ -0,0 +1,48 @@
+package org.prgrms.wumo.domain.member.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.prgrms.wumo.global.audit.BaseTimeEntity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Embedded
+	private Email email;
+
+	@Column(name = "nickname", nullable = false, updatable = true, unique = true)
+	private String nickname;
+
+	@Embedded
+	private Password password;
+
+	@Column(name = "image_url", nullable = true, updatable = true, unique = false)
+	private String profileImage;
+
+	//refreshToken
+
+	@Builder
+	public Member(Long id, String email, String nickname, String password, String profileImage) {
+		this.id = id;
+		this.email = new Email(email);
+		this.nickname = nickname;
+		this.password = new Password(password);
+		this.profileImage = profileImage;
+	}
+}
