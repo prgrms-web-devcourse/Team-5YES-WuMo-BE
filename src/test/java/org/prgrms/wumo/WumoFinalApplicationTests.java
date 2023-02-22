3b466602e30d920e651e65ded5c450e3880efde5
ttaehee <ttaehee1105@gmail.com>
2023-02-22 13:06:06 +0900

    Initial commit

---
+++ src/test/java/org/prgrms/wumo/WumoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgrms.wumo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WumoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
