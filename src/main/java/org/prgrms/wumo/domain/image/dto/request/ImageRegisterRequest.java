a3c6e87e6e9f4f384d373b84b6b53043cff1a163
김창규 <65993842+Kim-Changgyu@users.noreply.github.com>
2023-02-23 03:25:44 +0900

    Docs : [WUMO-19] 이미지 관리 API 명세서 추가 (#10)

---
+++ src/main/java/org/prgrms/wumo/domain/image/dto/request/ImageRegisterRequest.java
@@ -0,0 +1,17 @@
+package org.prgrms.wumo.domain.image.dto.request;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "이미지 등록 요청 정보")
+public record ImageRegisterRequest(
+
+		@NotNull(message = "등록할 이미지가 필요합니다.")
+		@Schema(description = "이미지 파일", required = true)
+		MultipartFile image
+
+) {
+}
