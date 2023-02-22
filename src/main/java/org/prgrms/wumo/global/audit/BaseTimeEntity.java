008f621ecb0681a7488b29735ff7a3dabdeb6e61
ttaehee <ttaehee1105@gmail.com>
2023-02-22 21:18:37 +0900

    Chore : BaseTimeEntity

---
+++ src/main/java/org/prgrms/wumo/global/audit/BaseTimeEntity.java
@@ -0,0 +1,27 @@
+package org.prgrms.wumo.global.audit;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+	@CreatedDate
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at", nullable = false, updatable = true)
+	private LocalDateTime updatedAt;
+}
