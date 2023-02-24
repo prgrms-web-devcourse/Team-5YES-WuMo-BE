c571d1198821ea3e1ed1709209254dde4ef4277a
ttaehee <ttaehee1105@gmail.com>
2023-02-24 23:45:05 +0900

    Test : [WUMO-118] Member 회원가입 기능 테스트

--- src/test/java/org/prgrms/wumo/domain/member/service/MemberServiceTest.java
+++ src/test/java/org/prgrms/wumo/domain/member/service/MemberServiceTest.java
@@ -1,6 +1,8 @@
 package org.prgrms.wumo.domain.member.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,7 +14,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.prgrms.wumo.domain.member.dto.request.MemberEmailCheckRequest;
 import org.prgrms.wumo.domain.member.dto.request.MemberNicknameCheckRequest;
+import org.prgrms.wumo.domain.member.dto.request.MemberRegisterRequest;
+import org.prgrms.wumo.domain.member.dto.response.MemberRegisterResponse;
 import org.prgrms.wumo.domain.member.model.Email;
+import org.prgrms.wumo.domain.member.model.Member;
 import org.prgrms.wumo.domain.member.repository.MemberRepository;
 import org.prgrms.wumo.global.exception.custom.DuplicateException;
 
@@ -27,11 +32,45 @@ public class MemberServiceTest {
 	MemberRepository memberRepository;
 
 	@Nested
+	@DisplayName("registerMember 메소드는 회원가입 요청 시 ")
+	class RegisterMember {
+
+		//given
+		String email = "5yes@gmail.com";
+		String nickname = "오예스오리지널";
+		String password = "qwe12345";
+
+		MemberRegisterRequest memberRegisterRequest
+			= new MemberRegisterRequest(email, nickname, password);
+
+		Member member = Member.builder()
+			.id(1L)
+			.email(email)
+			.nickname(nickname)
+			.password(password)
+			.build();
+
+		@Test
+		@DisplayName("이메일과 닉네임이 중복되지 않으면 성공한다")
+		void success() {
+			//mocking
+			given(memberRepository.save(any(Member.class)))
+				.willReturn(member);
+
+			//when
+			MemberRegisterResponse result = memberService.registerMember(memberRegisterRequest);
+
+			//then
+			assertThat(result.id()).isEqualTo(1L);
+		}
+	}
+
+	@Nested
 	@DisplayName("checkEmail 메소드는 이메일 중복체크 요청 시 ")
 	class CheckEmail {
 
 		//given
-		Email email = new Email("5yes@gmail.com");
+		String email = "5yes@gmail.com";
 
 		MemberEmailCheckRequest memberEmailCheckRequest
 			= new MemberEmailCheckRequest(email);
@@ -40,11 +79,11 @@ public class MemberServiceTest {
 		@DisplayName("이미 가입된 이메일이라면 예외가 발생한다")
 		void with_exist_emil() {
 			//mocking
-			given(memberRepository.existsByEmail(memberEmailCheckRequest.email()))
+			given(memberRepository.existsByEmail(new Email(memberEmailCheckRequest.email())))
 				.willReturn(true);
 
 			//when, then
-			assertThatThrownBy(() -> memberService.checkEmail(memberEmailCheckRequest))
+			assertThatThrownBy(() -> memberService.checkEmail(memberEmailCheckRequest.email()))
 				.isInstanceOf(DuplicateException.class)
 				.hasMessage("이메일이 중복됩니다.");
 		}
@@ -68,7 +107,7 @@ public class MemberServiceTest {
 				.willReturn(true);
 
 			//when, then
-			assertThatThrownBy(() -> memberService.checkNickname(memberNicknameCheckRequest))
+			assertThatThrownBy(() -> memberService.checkNickname(memberNicknameCheckRequest.nickname()))
 				.isInstanceOf(DuplicateException.class)
 				.hasMessage("닉네임이 중복됩니다.");
 		}
