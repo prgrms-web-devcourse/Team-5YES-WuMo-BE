76815f9ffd4c924b71956b1eb242fec869962fab
ttaehee <ttaehee1105@gmail.com>
2023-02-26 01:49:19 +0900

    Feat : [WUMO-143] JWT 토큰 관련 필터 구현

---
+++ src/main/java/org/prgrms/wumo/global/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,49 @@
+package org.prgrms.wumo.global.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+	private static final String AUTHORIZATION = "Authorization";
+	private static final String BEARER_TYPE = "Bearer";
+
+	private final JwtTokenProvider jwtTokenProvider;
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+		throws IOException, ServletException {
+
+		String token = extractToken((HttpServletRequest)request);
+
+		if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
+			Authentication authentication = jwtTokenProvider.getAuthentication(token);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+
+		chain.doFilter(request, response);
+	}
+
+	private String extractToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader(AUTHORIZATION);
+		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_TYPE)) {
+			return bearerToken.substring(7);
+		}
+		return null;
+	}
+}
