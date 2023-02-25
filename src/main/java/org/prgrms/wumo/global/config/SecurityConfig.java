87d01e4056436ec816438ab4acd0c6fc9f10efde
ttaehee <ttaehee1105@gmail.com>
2023-02-26 01:45:11 +0900

    Feat : [WUMO-142] JWT 토큰 생성 및 인증 객체 구현

--- src/main/java/org/prgrms/wumo/global/config/SecurityConfig.java
+++ src/main/java/org/prgrms/wumo/global/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package org.prgrms.wumo.global.config;
 
+import org.prgrms.wumo.global.jwt.JwtAuthenticationFilter;
+import org.prgrms.wumo.global.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +16,8 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final JwtTokenProvider jwtTokenProvider;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,8 +27,10 @@ public class SecurityConfig {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 			.authorizeHttpRequests()
-			.antMatchers("/swagger-ui.html").permitAll()
-			.and();
+			.antMatchers("/api/v1/members/signup", "/api/v1/members/login", "/swagger-ui.html")
+			.permitAll()
+			.and()
+			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
