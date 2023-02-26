5bb8cc35d80dc8e6cc1475b5e3a96a327a7e5ef7
김창규 <kimc980106@naver.com>
2023-02-26 22:18:41 +0900

    Feat : [WUMO-154] Party 사용자 기준 목록 조회 기능 구현
    - Member 기준 조회
    - 상세 조회가 아니기 때문에 식별자, 모임 이름, 커버 이미지만 포함

--- src/main/java/org/prgrms/wumo/domain/party/repository/PartyMemberRepository.java
+++ src/main/java/org/prgrms/wumo/domain/party/repository/PartyMemberRepository.java
@@ -1,7 +1,13 @@
 package org.prgrms.wumo.domain.party.repository;
 
+import java.util.List;
+
+import org.prgrms.wumo.domain.member.model.Member;
 import org.prgrms.wumo.domain.party.model.PartyMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PartyMemberRepository extends JpaRepository<PartyMember, Long> {
+
+	List<PartyMember> findAllByMember(Member member);
+
 }
