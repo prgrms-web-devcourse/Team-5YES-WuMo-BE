6d9d7df0c1d0201a5cb79f5fbc5b7a177eaeedcb
ttaehee <ttaehee1105@gmail.com>
2023-02-27 15:35:37 +0900

    Fix : application단에서 던지는 인가 예외 핸들링

--- src/main/java/org/prgrms/wumo/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/prgrms/wumo/global/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.prgrms.wumo.global.exception.custom.ImageDeleteFailedException;
 import org.prgrms.wumo.global.exception.custom.ImageUploadFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +14,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+	@ExceptionHandler({
+		AccessDeniedException.class
+	})
+	public ResponseEntity<ExceptionResponse> handleAccessException(RuntimeException runtimeException) {
+		log.info("exception : " + runtimeException);
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+			.body(new ExceptionResponse(runtimeException.getMessage()));
+	}
 
 	@ExceptionHandler({
 		DuplicateException.class
