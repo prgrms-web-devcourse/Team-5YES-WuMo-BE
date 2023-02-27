e95768c0254985f0e1a7b80cba668fd6289954e8
boompatron <grapeflavoredapple@gmail.com>
2023-02-27 14:23:32 +0900

    Test : [WUMO-45] Location 상세 조회 기능 Test

--- src/test/java/org/prgrms/wumo/domain/location/api/LocationControllerTest.java
+++ src/test/java/org/prgrms/wumo/domain/location/api/LocationControllerTest.java
@@ -1,5 +1,6 @@
 package org.prgrms.wumo.domain.location.api;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -10,30 +11,44 @@ import org.junit.jupiter.api.Test;
 import org.prgrms.wumo.MysqlTestContainer;
 import org.prgrms.wumo.domain.location.dto.request.LocationRegisterRequest;
 import org.prgrms.wumo.domain.location.model.Category;
+import org.prgrms.wumo.domain.location.model.Location;
+import org.prgrms.wumo.domain.location.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-@DisplayName("Location Controller 를 통해 ")
+@DisplayName("LocationController 를 통해 ")
 public class LocationControllerTest extends MysqlTestContainer {
 
 	@Autowired
 	MockMvc mockMvc;
 
 	@Autowired
-	private Jackson2ObjectMapperBuilder objectMapperBuilder;
+	LocationRepository locationRepository;
 
-	float longitude = 127.0283f;
-	float latitude = 37.4975f;
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	float longitude = 127.028f;
+	float latitude = 37.497f;
 	LocalDateTime dayToVisit = LocalDateTime.now().plusDays(10);
+
+	Location location = Location.builder()
+				.id(1L).image("http://programmers_gangnam_image.com")
+				.description("이번에 새로 오픈한 프로그래머스 강남 교육장!! 모니터도 있고 좋은데 화장실이 좀....")
+				.latitude(latitude).longitude(longitude)
+				.address("강남역 2번출구").visitDate(dayToVisit)
+				.category(Category.STUDY).name("프로그래머스 강남 교육장")
+				.spending(3000).expectedCost(4000).partyId(1L)
+				.build();
 
 	@Test
 	@DisplayName("후보 장소를 등록한다")
@@ -60,7 +75,7 @@ public class LocationControllerTest extends MysqlTestContainer {
 								.contentType(MediaType.APPLICATION_JSON_VALUE)
 								.characterEncoding("UTF-8")
 								.content(
-										objectMapperBuilder.build().writeValueAsString(locationRegisterRequest)
+										objectMapper.writeValueAsString(locationRegisterRequest)
 								)
 				);
 
@@ -70,5 +85,24 @@ public class LocationControllerTest extends MysqlTestContainer {
 				.andExpect(jsonPath("$.id").value(1))
 				.andDo(print());
 
+	}
+
+	@Test
+	@DisplayName("후보 장소 하나를 상세 조회할 수 있다.")
+	void getLocationTest() throws Exception {
+		// Given
+		locationRepository.save(location);
+
+		// When
+		ResultActions resultActions = mockMvc.perform(get("/api/v1/locations/{locationId}", 1));
+
+		// Then
+		resultActions
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").value(1))
+				.andExpect(jsonPath("$.name").value("프로그래머스 강남 교육장"))
+				.andExpect(jsonPath("$.latitude").value(latitude))
+				.andExpect(jsonPath("$.longitude").value(longitude))
+				.andDo(print());
 	}
 }
--- src/test/java/org/prgrms/wumo/domain/location/service/LocationServiceTest.java
+++ src/test/java/org/prgrms/wumo/domain/location/service/LocationServiceTest.java
@@ -1,6 +1,12 @@
 package org.prgrms.wumo.domain.location.service;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import java.time.LocalDateTime;
+import java.util.Optional;
+import javax.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -9,13 +15,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.prgrms.wumo.domain.location.dto.request.LocationRegisterRequest;
+import org.prgrms.wumo.domain.location.dto.response.LocationGetResponse;
 import org.prgrms.wumo.domain.location.dto.response.LocationRegisterResponse;
 import org.prgrms.wumo.domain.location.model.Category;
 import org.prgrms.wumo.domain.location.model.Location;
 import org.prgrms.wumo.domain.location.repository.LocationRepository;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("LocationService 에서 ")
@@ -27,15 +31,11 @@ public class LocationServiceTest {
 	@Mock
 	LocationRepository locationRepository;
 
-	float longitude = 127.0283f;
-	float latitude = 37.4975f;
+	float longitude = 127.028f;
+	float latitude = 37.497f;
 	LocalDateTime dayToVisit = LocalDateTime.now().plusDays(10);
 
-	@Nested
-	@DisplayName("후보 장소를 ")
-	class RegisterLocation{
-		// Given
-		Location location = Location.builder()
+	Location location = Location.builder()
 				.id(1L).image("http://programmers_gangnam_image.com")
 				.description("이번에 새로 오픈한 프로그래머스 강남 교육장!! 모니터도 있고 좋은데 화장실이 좀....")
 				.latitude(latitude).longitude(longitude)
@@ -44,6 +44,10 @@ public class LocationServiceTest {
 				.spending(3000).expectedCost(4000)
 				.build();
 
+	@Nested
+	@DisplayName("registerLocation을 사용해서 ")
+	class RegisterLocation{
+		// Given
 		LocationRegisterRequest locationRegisterRequest
 				= new LocationRegisterRequest(
 						"프로그래머스 강남 교육장", "강남역 2번출구"
@@ -53,7 +57,7 @@ public class LocationServiceTest {
 		);
 
 		@Test
-		@DisplayName("성공적으로 등록한다")
+		@DisplayName("후보 장소를 등록할 수 있다.")
 		void registerLocationTest(){
 			// Given
 			given(locationRepository.save(any(Location.class))).willReturn(location);
@@ -65,6 +69,45 @@ public class LocationServiceTest {
 			// Then
 			assertThat(locationRegisterResponse.id()).isEqualTo(1L);
 
+		}
+	}
+
+	@Nested
+	@DisplayName("getLocation을 사용해서 ")
+	class GetLocation{
+		// Given
+
+		@Test
+		@DisplayName("후보 장소 하나를 상세 조회할 수 있다.")
+		void getOneLocationTest(){
+			// Given
+			given(locationRepository.findById(1L)).willReturn(Optional.of(location));
+
+			// When
+			LocationGetResponse locationGetResponse =
+					locationService.getLocation(1L);
+
+			// Then
+			assertThat(locationGetResponse.id()).isEqualTo(1L);
+			assertThat(locationGetResponse.latitude()).isEqualTo(latitude);
+			assertThat(locationGetResponse.longitude()).isEqualTo(longitude);
+			assertThat(locationGetResponse.spending()).isEqualTo(3000);
+			assertThat(locationGetResponse.expectedCost()).isEqualTo(4000);
+			assertThat(locationGetResponse.visitDate()).isEqualTo(dayToVisit);
+			assertThat(locationGetResponse.category()).isEqualTo(Category.STUDY);
+			assertThat(locationGetResponse.name()).isEqualTo("프로그래머스 강남 교육장");
+		}
+
+		@Test
+		@DisplayName(" 없는 후보 장소는 상세 조회할 수 없다.")
+		void getLocationFailTest(){
+			// Given
+			given(locationRepository.findById(1L)).willReturn(Optional.empty());
+
+			// When // Then
+			assertThatThrownBy(
+					() -> locationService.getLocation(1L)
+			).isInstanceOf(EntityNotFoundException.class);
 		}
 	}
 }
