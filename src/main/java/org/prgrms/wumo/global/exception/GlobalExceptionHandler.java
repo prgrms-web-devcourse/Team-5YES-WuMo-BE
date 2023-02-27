02e5aff2e6095dee9deb7617eed84054cbf6032c
ttaehee <ttaehee1105@gmail.com>
2023-02-27 15:35:14 +0900

    Refactor : 이메일, 닉네임 중복 시 반환하는 Http Status code를 bad request에서 conflict로 수정

--- src/main/java/org/prgrms/wumo/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/prgrms/wumo/global/exception/GlobalExceptionHandler.java
@@ -1,10 +1,9 @@
 package org.prgrms.wumo.global.exception;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.prgrms.wumo.global.exception.custom.DuplicateException;
 import org.prgrms.wumo.global.exception.custom.ImageDeleteFailedException;
 import org.prgrms.wumo.global.exception.custom.ImageUploadFailedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,12 +15,21 @@ import lombok.extern.slf4j.Slf4j;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler({
-			DuplicateException.class, EntityNotFoundException.class,
-			ImageUploadFailedException.class, IllegalArgumentException.class, ImageDeleteFailedException.class
+		DuplicateException.class
+	})
+	public ResponseEntity<ExceptionResponse> handleDuplicateException(RuntimeException runtimeException) {
+		log.info("exception : " + runtimeException);
+		return ResponseEntity.status(HttpStatus.CONFLICT)
+			.body(new ExceptionResponse(runtimeException.getMessage()));
+	}
+
+	@ExceptionHandler({
+		ImageUploadFailedException.class, IllegalArgumentException.class, ImageDeleteFailedException.class
 	})
 	public ResponseEntity<ExceptionResponse> handleException(RuntimeException runtimeException) {
 		log.info("exception : " + runtimeException);
-		return ResponseEntity.badRequest().body(new ExceptionResponse(runtimeException.getMessage()));
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(new ExceptionResponse(runtimeException.getMessage()));
 	}
 
 }
