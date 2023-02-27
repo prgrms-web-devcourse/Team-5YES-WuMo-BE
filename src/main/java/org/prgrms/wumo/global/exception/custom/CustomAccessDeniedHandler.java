163d345b5a5062ff48a643ead48faa5067911e03
ttaehee <ttaehee1105@gmail.com>
2023-02-27 15:11:41 +0900

    Feat : [WUMO-167] 인가 관련 예외 구현

---
+++ src/main/java/org/prgrms/wumo/global/exception/custom/CustomAccessDeniedHandler.java
@@ -0,0 +1,38 @@
+package org.prgrms.wumo.global.exception.custom;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.prgrms.wumo.global.exception.ExceptionResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+	private final ObjectMapper objectMapper;
+
+	@Override
+	public void handle(
+		HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException)
+		throws IOException {
+
+		log.info("No Authorities", accessDeniedException);
+		ExceptionResponse exceptionResponse = new ExceptionResponse("없는 리소스입니다.");
+
+		String responseBody = objectMapper.writeValueAsString(exceptionResponse);
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(responseBody);
+	}
+}
