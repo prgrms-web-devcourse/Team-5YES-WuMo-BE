05dd50a6327a0a9a4fe7d6608b89b797318b072b
boompatron <grapeflavoredapple@gmail.com>
2023-02-28 14:20:09 +0900

    Fix : [WUMO-188] Builder 관련 오류 수정

--- src/main/java/org/prgrms/wumo/domain/comment/model/LocationComment.java
+++ src/main/java/org/prgrms/wumo/domain/comment/model/LocationComment.java
@@ -1,19 +1,17 @@
 package org.prgrms.wumo.domain.comment.model;
 
 import static lombok.AccessLevel.PROTECTED;
-
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-import org.prgrms.wumo.domain.member.model.Member;
-import org.prgrms.wumo.domain.party.model.PartyMember;
-
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.prgrms.wumo.domain.member.model.Member;
+import org.prgrms.wumo.domain.party.model.PartyMember;
 
 @Getter
 @Entity
@@ -29,11 +27,15 @@ public class LocationComment extends Comment {
 	@JoinColumn(name = "party_member_id")
 	private PartyMember partyMember;
 
-	//@Builder
+	@Builder
 	public LocationComment(Long id, Member member, String content, String image, Long locationId,
-		PartyMember partyMember, boolean isEdited) {
+			PartyMember partyMember, boolean isEdited) {
 		super(id, member, content, image, isEdited);
 		this.locationId = locationId;
+		this.partyMember = partyMember;
+	}
+
+	public void setPartyMember(PartyMember partyMember) {
 		this.partyMember = partyMember;
 	}
 }
--- src/main/java/org/prgrms/wumo/domain/comment/model/PartyRouteComment.java
+++ src/main/java/org/prgrms/wumo/domain/comment/model/PartyRouteComment.java
@@ -1,19 +1,17 @@
 package org.prgrms.wumo.domain.comment.model;
 
 import static lombok.AccessLevel.PROTECTED;
-
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-import org.prgrms.wumo.domain.member.model.Member;
-import org.prgrms.wumo.domain.party.model.PartyMember;
-
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.prgrms.wumo.domain.member.model.Member;
+import org.prgrms.wumo.domain.party.model.PartyMember;
 
 @Getter
 @Entity
@@ -29,9 +27,9 @@ public class PartyRouteComment extends Comment {
 	@JoinColumn(name = "party_member_id")
 	private PartyMember partyMember;
 
-	//@Builder
+	@Builder
 	public PartyRouteComment(Long id, Member member, String content, String image, Long routeId,
-		PartyMember partyMember, boolean isEdited) {
+			PartyMember partyMember, boolean isEdited) {
 		super(id, member, content, image, isEdited);
 		this.routeId = routeId;
 		this.partyMember = partyMember;
