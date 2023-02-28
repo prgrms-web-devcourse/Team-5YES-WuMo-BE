11fe4259c4b7e84d76f6c85da3473318055bebad
Taehee Kim <ttaehee1105@gmail.com>
2023-02-28 21:17:31 +0900

    [WUMO-200] Security CORS 활성화 (#56)
    * Chore : [WUMO-201] Spring Security에서 cors 활성화

--- src/main/java/org/prgrms/wumo/global/config/SecurityConfig.java
+++ src/main/java/org/prgrms/wumo/global/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.prgrms.wumo.global.config;
 
+import java.util.Arrays;
+
 import org.prgrms.wumo.global.exception.custom.CustomAccessDeniedHandler;
 import org.prgrms.wumo.global.exception.custom.CustomAuthenticationEntryPoint;
 import org.prgrms.wumo.global.jwt.JwtAuthenticationFilter;
@@ -8,9 +10,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -23,6 +29,16 @@ public class SecurityConfig {
 
 	private final JwtConfig jwtConfig;
 	private final ObjectMapper objectMapper;
+
+	private final String[] apiUrls = {
+		"/docs", "/docs/index.html", "/docs/**", "/index.html", "/swagger-ui.html"
+	};
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return web -> web.ignoring()
+			.antMatchers(apiUrls);
+	}
 
 	@Bean
 	public JwtTokenProvider jwtTokenProvider() {
@@ -41,21 +57,43 @@ public class SecurityConfig {
 	}
 
 	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+		corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("POST", "GET", "DELETE", "PUT", "PATCH"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+		corsConfiguration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return urlBasedCorsConfigurationSource;
+	}
+
+	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf().disable()
 			.httpBasic().disable()
+			.formLogin().disable()
+			.rememberMe().disable()
+			.logout().disable()
+
+			.cors()
+			.configurationSource(corsConfigurationSource())
+			.and()
+
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
+
 			.authorizeHttpRequests()
-			.antMatchers(
-				"/api/v1/members/signup",
-				"/api/v1/members/login",
-				"/swagger-ui.html")
-			.permitAll()
+			.antMatchers(apiUrls).permitAll()
+			.antMatchers("/api/v1/members/signup", "/api/v1/members/login").permitAll()
 			.and()
+
 			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider()),
 				UsernamePasswordAuthenticationFilter.class)
+
 			.exceptionHandling()
 			.authenticationEntryPoint(customAuthenticationEntryPoint())
 			.accessDeniedHandler(customAccessDeniedHandler());
