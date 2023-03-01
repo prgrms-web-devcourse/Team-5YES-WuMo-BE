d7e7f4549c45947fbb8aac78975a47853ec47955
ttaehee <ttaehee1105@gmail.com>
2023-03-01 16:57:06 +0900

    Docs : [WUMO-211] 루트 API 명세서 단어 통일 및 형식 정리

--- src/main/java/org/prgrms/wumo/domain/route/dto/request/RouteStatusUpdateRequest.java
+++ src/main/java/org/prgrms/wumo/domain/route/dto/request/RouteStatusUpdateRequest.java
@@ -7,10 +7,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(name = "루트 공개여부 변경 요청 정보")
 public record RouteStatusUpdateRequest(
 	@NotNull(message = "공개할 루트를 선택해주세요.")
-	@Schema(description = "공개할 루트 식별자", required = true, example = "1")
+	@Schema(description = "공개할 루트 식별자", example = "1", required = true)
 	Long routeId,
 
-	@Schema(description = "루트 공개여부(true면 공개)", required = true, example = "true")
+	@Schema(description = "루트 공개여부(true면 공개)", example = "1", required = true)
 	boolean isPublic
 ) {
 }
--- src/main/java/org/prgrms/wumo/domain/route/dto/response/RouteGetAllResponse.java
+++ src/main/java/org/prgrms/wumo/domain/route/dto/response/RouteGetAllResponse.java
@@ -10,16 +10,16 @@ public record RouteGetAllResponse(
 	// @Schema(description = "루트의 좋아요 수", example = "10", required = true)
 	// long likeCount,
 
-	@Schema(description = "루트의 장소 이름들", example = "[{갈치 구이집}, {바다 카페}]", required = true)
+	@Schema(description = "루트의 장소 이름들", required = true)
 	List<RouteLocationSimpleResponse> locations,
 
 	@Schema(description = "루트 이름", example = "퇴사 기념 여행", required = true)
 	String name,
 
-	@Schema(description = "루트가 속한 모임 시작일", example = "퇴사 기념 여행", required = true)
+	@Schema(description = "루트가 속한 모임 시작일", example = "2023-02-21T10:00:00", required = true)
 	LocalDateTime startDate,
 
-	@Schema(description = "루트가 속한 모임 마지막일", example = "퇴사 기념 여행", required = true)
+	@Schema(description = "루트가 속한 모임 마지막일", example = "2023-02-25T10:00:00", required = true)
 	LocalDateTime endDate
 ) {
 }
