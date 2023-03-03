8c23a2307b0f06ab935c42efc4b9131c4c1aeb87
boompatron <grapeflavoredapple@gmail.com>
2023-03-03 21:40:30 +0900

    Feat : [WUMO-219] 후보지 목록 조회 방식 수정

--- src/main/java/org/prgrms/wumo/domain/location/repository/LocationRepository.java
+++ src/main/java/org/prgrms/wumo/domain/location/repository/LocationRepository.java
@@ -1,14 +1,9 @@
 package org.prgrms.wumo.domain.location.repository;
 
-import java.util.List;
 import org.prgrms.wumo.domain.location.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LocationRepository extends JpaRepository<Location, Long> {
-
-	@Query(value = "SELECT * FROM location WHERE party_id = :partyId AND id > :cursorId LIMIT :pageSize", nativeQuery = true)
-	List<Location> findAllByPartyIdAndCursorIdLimitPageSize(Long partyId, Long cursorId, int pageSize);
+public interface LocationRepository extends JpaRepository<Location, Long>, LocationCustomRepository {
 }
