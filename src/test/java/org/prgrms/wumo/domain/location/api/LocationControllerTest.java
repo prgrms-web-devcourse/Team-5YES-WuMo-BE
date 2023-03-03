2a6f476c6fc3b8b943813aa5c291a25c9777454d
boompatron <grapeflavoredapple@gmail.com>
2023-03-04 02:02:57 +0900

    Test : [WUMO-235] 후보지 삭제 기능 Test

--- src/test/java/org/prgrms/wumo/domain/location/api/LocationControllerTest.java
+++ src/test/java/org/prgrms/wumo/domain/location/api/LocationControllerTest.java
@@ -72,7 +72,7 @@ public class LocationControllerTest extends MysqlTestContainer {
 	PartyMember partyMember;
 
 	@BeforeEach
-	void beforeEach(){
+	void beforeEach() {
 		member = memberRepository.save(
 				Member.builder()
 						.password("qwe12345")
@@ -121,36 +121,36 @@ public class LocationControllerTest extends MysqlTestContainer {
 	void registerLocationTest() throws Exception {
 		// Given
 		LocationRegisterRequest locationRegisterRequest
-			= new LocationRegisterRequest(
-					"프로그래머스 강남 교육장",
-			"서울특별시",
-			"강남역 2번출구 올라가기 전",
-			locationTestUtils.getLatitude1(),
-			locationTestUtils.getLongitude1(),
-			"http://programmers_gangnam_image.com",
-			Category.STUDY,
-			"이번에 새로 오픈한 프로그래머스 강남 교육장!! 모니터도 있고 좋은데 화장실이 좀....",
-			locationTestUtils.getDayToVisit()
-			, 4000,
-			party.getId()
+				= new LocationRegisterRequest(
+				"프로그래머스 강남 교육장",
+				"서울특별시",
+				"강남역 2번출구 올라가기 전",
+				locationTestUtils.getLatitude1(),
+				locationTestUtils.getLongitude1(),
+				"http://programmers_gangnam_image.com",
+				Category.STUDY,
+				"이번에 새로 오픈한 프로그래머스 강남 교육장!! 모니터도 있고 좋은데 화장실이 좀....",
+				locationTestUtils.getDayToVisit(),
+				4000,
+				party.getId()
 		);
 
 		// When
 		ResultActions resultActions =
-			mockMvc.perform(
-				post("/api/v1/locations")
-					.contentType(MediaType.APPLICATION_JSON_VALUE)
-					.characterEncoding("UTF-8")
-					.content(
-						objectMapper.writeValueAsString(locationRegisterRequest)
-					)
-			);
+				mockMvc.perform(
+						post("/api/v1/locations")
+								.contentType(MediaType.APPLICATION_JSON_VALUE)
+								.characterEncoding("UTF-8")
+								.content(
+										objectMapper.writeValueAsString(locationRegisterRequest)
+								)
+				);
 
 		// Then
 		resultActions
-			.andExpect(status().isCreated())
-			.andExpect(jsonPath("$.id").isNotEmpty())
-			.andDo(print());
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.id").isNotEmpty())
+				.andDo(print());
 
 	}
 
@@ -165,12 +165,12 @@ public class LocationControllerTest extends MysqlTestContainer {
 
 		// Then
 		resultActions
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.id").isNotEmpty())
-			.andExpect(jsonPath("$.name").value("프로그래머스 강남 교육장"))
-			.andExpect(jsonPath("$.latitude").value(locationTestUtils.getLatitude1()))
-			.andExpect(jsonPath("$.longitude").value(locationTestUtils.getLongitude1()))
-			.andDo(print());
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").isNotEmpty())
+				.andExpect(jsonPath("$.name").value("프로그래머스 강남 교육장"))
+				.andExpect(jsonPath("$.latitude").value(locationTestUtils.getLatitude1()))
+				.andExpect(jsonPath("$.longitude").value(locationTestUtils.getLongitude1()))
+				.andDo(print());
 	}
 
 	@Test
@@ -182,19 +182,19 @@ public class LocationControllerTest extends MysqlTestContainer {
 
 		// When
 		ResultActions resultActions = mockMvc.perform(
-			get("/api/v1/locations")
-				.param("cursorId", (String)null)
-				.param("pageSize", "5")
-				.param("partyId", "1")
+				get("/api/v1/locations")
+						.param("cursorId", (String)null)
+						.param("pageSize", "5")
+						.param("partyId", "1")
 		);
 
 		// Then
 		resultActions
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.locations").isArray())
-			.andExpect(jsonPath("$.locations").isNotEmpty())
-			.andExpect(jsonPath("$.locations[0].id").isNotEmpty())
-			.andDo(print());
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.locations").isArray())
+				.andExpect(jsonPath("$.locations").isNotEmpty())
+				.andExpect(jsonPath("$.locations[0].id").isNotEmpty())
+				.andDo(print());
 	}
 
 	@Test
@@ -219,15 +219,49 @@ public class LocationControllerTest extends MysqlTestContainer {
 		);
 
 		Long locationId = locationRepository.save(location).getId();
+
 		// When
 		ResultActions resultActions = mockMvc.perform(
-			delete("/api/v1/locations")
-				.param("locationId", String.valueOf(locationId))
+				delete("/api/v1/locations")
+						.param("locationId", String.valueOf(locationId))
 		);
 
 		// Then
 		resultActions
-			.andExpect(status().isOk())
-			.andDo(print());
+				.andExpect(status().isOk())
+				.andDo(print());
+	}
+
+	@Test
+	@DisplayName(" 후보지를 삭제한다.")
+	void deleteLocation() throws Exception {
+		// Given
+		Long locationId = locationRepository.save(
+				Location.builder()
+						.category(Category.COFFEE)
+						.visitDate(locationTestUtils.getDayToVisit())
+						.description("아인슈페너가 맛있는 곳!")
+						.name("cafe")
+						.searchAddress("고양시")
+						.address("경기도 고양시 일산서구")
+						.latitude(12.34F)
+						.longitude(34.56F)
+						.partyId(party.getId())
+						.expectedCost(4000)
+						.spending(3500)
+						.image("image.url")
+						.build()
+		).getId();
+
+		// When
+		ResultActions resultActions =
+				mockMvc.perform(
+						delete("/api/v1/locations/{locationId}", locationId)
+				);
+
+		// Then
+		resultActions
+				.andExpect(status().isOk())
+				.andDo(print());
 	}
 }
