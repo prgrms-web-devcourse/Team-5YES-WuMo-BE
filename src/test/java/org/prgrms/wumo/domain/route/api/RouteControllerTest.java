583ea0731359e6c808ccfb91f26019e20a25513b
ttaehee <ttaehee1105@gmail.com>
2023-03-03 16:22:13 +0900

    Test : [WUMO-228] 루트 검색 기능 테스트

--- src/test/java/org/prgrms/wumo/domain/route/api/RouteControllerTest.java
+++ src/test/java/org/prgrms/wumo/domain/route/api/RouteControllerTest.java
@@ -89,6 +89,8 @@ public class RouteControllerTest extends MysqlTestContainer {
 
 		Route route = routeRepository.save(getRouteData(location, party));
 		routeId = route.getId();
+		route.updatePublicStatus(true);
+		routeRepository.save(route);
 
 		SecurityContext context = SecurityContextHolder.getContext();
 		UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
@@ -174,13 +176,15 @@ public class RouteControllerTest extends MysqlTestContainer {
 		ResultActions resultActions
 			= mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/routes")
 			.param("cursorId", (String)null)
-			.param("pageSize", String.valueOf(pageSize)));
+			.param("pageSize", String.valueOf(pageSize))
+			.param("searchWord", (String)null));
 
 		//then
 		resultActions
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.lastId").isNotEmpty())
+			.andExpect(jsonPath("$.lastId").value(routeId))
 			.andExpect(jsonPath("$.routes").isArray())
+			.andExpect(jsonPath("$.routes[0].routeId").value(routeId))
 			.andDo(print());
 	}
 
