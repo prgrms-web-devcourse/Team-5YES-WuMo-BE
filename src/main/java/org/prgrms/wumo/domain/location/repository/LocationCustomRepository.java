626d946d90da7fd7ea4242a3893ddb7647b14e8c
boompatron <grapeflavoredapple@gmail.com>
2023-03-03 21:40:30 +0900

    Feat : [WUMO-219] 후보지 목록 조회 방식 수정

---
+++ src/main/java/org/prgrms/wumo/domain/location/repository/LocationCustomRepository.java
@@ -0,0 +1,10 @@
+package org.prgrms.wumo.domain.location.repository;
+
+import java.util.List;
+
+import org.prgrms.wumo.domain.location.model.Location;
+
+public interface LocationCustomRepository {
+
+	List<Location> findByPartyId(Long cursorId, int pageSize, Long partyId);
+}
