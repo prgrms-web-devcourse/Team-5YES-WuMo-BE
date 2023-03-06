9d0263d7fb33eab77a6999edd3b9afef7360bac9
김창규 <kimc980106@naver.com>
2023-03-06 14:55:39 +0900

    Test : [WUMO-263] Image 삭제 중 도메인 검증 기능 테스트

--- src/test/java/org/prgrms/wumo/domain/image/api/ImageControllerTest.java
+++ src/test/java/org/prgrms/wumo/domain/image/api/ImageControllerTest.java
@@ -8,9 +8,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.prgrms.wumo.MysqlTestContainer;
 import org.prgrms.wumo.domain.image.dto.request.ImageDeleteRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -25,13 +25,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @DisplayName("ImageController 를 통해 ")
-class ImageControllerTest extends MysqlTestContainer {
+class ImageControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Autowired
 	private ObjectMapper objectMapper;
+
+	@Value("${cloud.aws.s3.bucket:#{null}}")
+	private String bucket;
+
+	@Value("${cloud.aws.region.static:#{null}}")
+	private String region;
 
 	@Test
 	@DisplayName("Content-Type 이 image/* 인 이미지를 저장할 수 있다.")
@@ -78,10 +84,10 @@ class ImageControllerTest extends MysqlTestContainer {
 
 	@Test
 	@DisplayName("이미지 URL을 통해 버킷에서 이미지를 삭제할 수 있다.")
-	void deleteImage() throws Exception {
+	void deleteImageFromValidHost() throws Exception {
 		//given (유효하지 않은 이미지 경로 사용)
 		ImageDeleteRequest imageDeleteRequest
-				= new ImageDeleteRequest("https://localhost/2022-02-24/image.png");
+				= new ImageDeleteRequest(String.format("https://%s.s3.%s.amazonaws.com/test.png", bucket, region));
 
 		//when
 		ResultActions resultActions = mockMvc.perform(delete("/api/v1/images")
@@ -90,7 +96,26 @@ class ImageControllerTest extends MysqlTestContainer {
 
 		//then
 		resultActions
-				.andExpect(status().isOk());
+				.andExpect(status().isOk())
+				.andDo(print());
+	}
+
+	@Test
+	@DisplayName("버킷의 이미지 URL이 아니면 오류가 발생한다.")
+	void deleteImageFromInvalidHost() throws Exception {
+		//given (유효하지 않은 이미지 경로 사용)
+		ImageDeleteRequest imageDeleteRequest
+				= new ImageDeleteRequest("http://localhost/test.png");
+
+		//when
+		ResultActions resultActions = mockMvc.perform(delete("/api/v1/images")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(objectMapper.writeValueAsString(imageDeleteRequest)));
+
+		//then
+		resultActions
+				.andExpect(status().isBadRequest())
+				.andDo(print());
 	}
 
 }
