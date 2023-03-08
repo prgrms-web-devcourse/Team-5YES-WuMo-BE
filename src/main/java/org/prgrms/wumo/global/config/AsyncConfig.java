0068058b4deb38e039276d8819ed437806bbbf7e
ttaehee <ttaehee1105@gmail.com>
2023-03-09 01:43:35 +0900

    Chore : Async 설정 추가
    - SimpleAsyncTaskExecutor 사용하지 않기 위해 설정 customizing

--- src/main/java/org/prgrms/wumo/global/config/AsyncConfig.java
+++ src/main/java/org/prgrms/wumo/global/config/AsyncConfig.java
@@ -1,9 +1,23 @@
 package org.prgrms.wumo.global.config;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
+
+	@Override
+	public Executor getAsyncExecutor() {
+		ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+		threadPoolTaskExecutor.setCorePoolSize(3);
+		threadPoolTaskExecutor.setMaxPoolSize(20);
+		threadPoolTaskExecutor.setQueueCapacity(20);
+		threadPoolTaskExecutor.initialize();
+		return threadPoolTaskExecutor;
+	}
 }
