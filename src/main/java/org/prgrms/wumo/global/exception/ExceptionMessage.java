4ba77bba7f3adbdb8be613d2a7499c438cbfc45b
ttaehee <ttaehee1105@gmail.com>
2023-03-10 21:49:42 +0900

    Chore : [WUMO-381] 예외메시지 enum class 구현

---
+++ src/main/java/org/prgrms/wumo/global/exception/ExceptionMessage.java
@@ -0,0 +1,18 @@
+package org.prgrms.wumo.global.exception;
+
+public enum ExceptionMessage {
+	MEMBER("회원"),
+	PARTY("모임"),
+	PARTY_MEMBER("모임 멤버"),
+	LOCATION("후보지"),
+	ROUTE("루트"),
+	COMMENT("댓글"),
+	ENTITY_NOT_FOUND("존재하지 않는 %s 입니다."),
+	ACCESS_DENIED("잘못된 접근입니다.");
+
+	private final String message;
+
+	ExceptionMessage(String message) {
+		this.message = message;
+	}
+}
