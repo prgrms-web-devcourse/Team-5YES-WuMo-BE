a8933a651d9578e80951f21a5ad3f5de11020326
ttaehee <ttaehee1105@gmail.com>
2023-03-13 12:25:55 +0900

    Refactor : [WUMO-410] 루트에 후보지 등록 요청 시 루트 식별자에서 모임 식별자로 요청하도록 수정
    - 프론트에서 루트 식별자를 추출할 방법이 상황에 따라 어려운 경우가 존재해 수정

--- src/main/java/org/prgrms/wumo/domain/route/dto/request/RouteRegisterRequest.java
+++ src/main/java/org/prgrms/wumo/domain/route/dto/request/RouteRegisterRequest.java
@@ -6,9 +6,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "루트에 후보지 등록 요청 정보")
 public record RouteRegisterRequest(
-	@Schema(description = "이미 루트가 존재할 경우의 루트 식별자(루트를 처음 생성한다면 null)", example = "1", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-	Long routeId,
-
 	@NotNull(message = "등록할 후보지를 선택해주세요.")
 	@Schema(description = "루트에 등록할 후보지 식별자", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
 	Long locationId,
