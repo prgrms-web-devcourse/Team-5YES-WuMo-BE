de22d335edcc9450fa76ac517805bf4fd7eb5fee
ttaehee <ttaehee1105@gmail.com>
2023-03-14 15:23:35 +0900

    Refactor : [WUMO-435] security 관련 파일들 빈주입에서 생성자 주입으로 변경

--- src/main/java/org/prgrms/wumo/global/jwt/CustomAuthenticationEntryPoint.java
+++ src/main/java/org/prgrms/wumo/global/jwt/CustomAuthenticationEntryPoint.java
@@ -9,6 +9,7 @@ import org.prgrms.wumo.global.exception.ExceptionResponse;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
